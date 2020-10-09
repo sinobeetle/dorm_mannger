63698196a5a75940de5a91bbffa43f1d4d255ccd
581004@qq.com <581004@qq.com>
2020-10-09 18:43:25 +0800

    11

--- src/main/java/ccc.java
+++
@@ -1,9 +0,0 @@
-import sqldao.*;
-
-import java.sql.SQLException;
-
-public class ccc {
-    public static void main(String[] args) throws SQLException {
-        sqlconnection.getsqlconnection();
-    }
-}
